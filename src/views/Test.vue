fb9fc8fb9b45c239a4a7f45cb7779dc48a2b6ff0
pjonasson <88997326+pjonasson@users.noreply.github.com>
2021-10-06 10:45:48 -0500

    Added Sign up, Login, and Logout functions

---
+++ src/views/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    <h1>This is my testing page</h1>
+    <button v-on:click="homePage">Click here to see this!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  created: function () {},
+  methods: {
+    homePage() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
